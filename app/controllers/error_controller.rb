9879bedee335c84f9e707d3a041ff993be2df6ef
Youssef Fares <youssefe.fares@gmail.com>
2017-08-31 13:20:03 +0200

    added rubocop and fixed some style issues (#1)
    * added rubocop and fixed some style issues
    
    * added circle ci configs
    
    * moved seeds.rb to its right folder
    
    * test commit
    
    * added pronto and configured circleci to use it
    
    * added rubocop and fixed some style issues
    
    * test commit
    
    * added dependency in circle ci script
    
    * added dependency in circle ci script
    
    * updated gems

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -7,6 +7,7 @@ class ErrorController < ApplicationController
   end
 
   def destroy
+      
     @error = Error.find(params[:id])
     @error.destroy
 
@@ -14,24 +15,23 @@ class ErrorController < ApplicationController
   end
 
   def help
-    #error message from form text area
+    # error message from form text area
     @message = params[:error]
 
-    #get error-help from python-error-helper api
-    uri = URI("http://python-error-helper.herokuapp.com/error-help")
+    # get error-help from python-error-helper apiuri = URI("http://python-error-helper.herokuapp.com/error-help")
     response = Net::HTTP.post_form(uri, 'error_message' => @message)
 
-    #get id & help (available to view)
-    @id, @help = JSON.parse(response.body).values_at("id", "response")
+    # get id & help (available to view)
+    @id, @help = JSON.parse(response.body).values_at('id', 'response')
 
-    #if new uncovered error message, add it to our table.
+    # if new uncovered error message, add it to our table.
     if @id == -1
       @uncovered_error = Error.new(message: @message)
       @uncovered_error.save
     end
 
-    #render same page & the view file will take care of rendering new things
-    render :file => '../views/error/index.html.erb'
+    # render same page & the view file will take care of rendering new things
+    render file: '../views/error/index.html.erb'
   end
 
   def list
