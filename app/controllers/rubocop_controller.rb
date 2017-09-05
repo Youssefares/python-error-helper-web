014c8b5bdd2bf5fedfb8f1b94d65119f1b2e9a93
youssefares <youssefe.fares@gmail.com>
2017-09-05 16:11:49 +0200

    add rest request

--- app/controllers/rubocop_controller.rb
+++ app/controllers/rubocop_controller.rb
@@ -3,7 +3,7 @@ class RubocopController < ApplicationController
 
   def webhook
     RestClient.post(
-      'https://api.github.com/repos/Youssefares/python-error-helper-web/statuses/#{params[:sha]}',
+      "https://api.github.com/repos/Youssefares/python-error-helper-web/statuses/#{params[:sha]}",
       { 'state': 'success' }.to_json,
       content_type: :json, accept: 'application/vnd.github.v3+json'
     )
