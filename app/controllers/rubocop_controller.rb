a2e15726796c8d5d601f7fa10427814f44bc8273
youssefares <youssefe.fares@gmail.com>
2017-09-05 16:29:21 +0200

    add rest request

--- app/controllers/rubocop_controller.rb
+++ app/controllers/rubocop_controller.rb
@@ -6,7 +6,7 @@ class RubocopController < ApplicationController
       response = RestClient.post(
         "https://api.github.com/repos/Youssefares/python-error-helper-web/statuses/#{params[:sha]}",
         { 'state': 'success' }.to_json,
-        {content_type: :json, accept: 'application/vnd.github.v3+json'}
+        { accept: 'application/vnd.github.v3+json'}
       )
     rescue RestClient::ExceptionWithResponse => e
       byebug
