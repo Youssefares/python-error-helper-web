e1a73d587e794c05114fd5d8a992cafaa37e353e
youssefares <youssefe.fares@gmail.com>
2017-09-05 16:14:38 +0200

    add rest request

--- app/controllers/rubocop_controller.rb
+++ app/controllers/rubocop_controller.rb
@@ -2,11 +2,14 @@ class RubocopController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def webhook
-    response = RestClient.post(
-      "https://api.github.com/repos/Youssefares/python-error-helper-web/statuses/#{params[:sha]}",
-      { 'state': 'success' }.to_json,
-      content_type: :json, accept: 'application/vnd.github.v3+json'
-    )
-    byebug
+    begin
+      response = RestClient.post(
+        "https://api.github.com/repos/Youssefares/python-error-helper-web/statuses/#{params[:sha]}",
+        { 'state': 'success' }.to_json,
+        content_type: :json, accept: 'application/vnd.github.v3+json'
+      )
+    rescue
+      byebug
+    end
   end
 end
